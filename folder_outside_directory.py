84f3ca5733b331198633627589c3a78945a81070
Antonia Still <cemast@cem.local>
2024-05-06 12:08:17 +0100

    Add docstrings

--- folder_outside_directory.py
+++ folder_outside_directory.py
@@ -1,6 +1,19 @@
 import os
 
 def add_new_folder(folder_path, folder_name):
+    """
+    Create a new folder in the specified directory.
+
+    Args:
+        folder_path (str): The path of the directory where the new folder will be created.
+        folder_name (str): The name of the new folder.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
     if os.path.isdir(folder_path) == False:
         print(f'This folder path: {folder_path} does not exist')
         return
@@ -11,6 +24,19 @@ def add_new_folder(folder_path, folder_name):
         os.mkdir(f'{folder_path}/{folder_name}')
 
 def remove_folder(folder_path, folder_name):
+    """
+    Remove a folder from the specified directory.
+
+    Args:
+        folder_path (str): The path of the directory containing the folder.
+        folder_name (str): The name of the folder to be removed.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
     if os.path.isdir(folder_path) == False:
         print(f'This folder path: {folder_path} does not exist')
         return
@@ -21,6 +47,20 @@ def remove_folder(folder_path, folder_name):
         os.rmdir(f'{folder_path}/{folder_name}')
 
 def rename_folder(folder_path, old_folder_name, new_folder_name):
+    """
+    Renames a folder in the specified directory.
+
+    Args:
+        folder_path (str): The path to the directory containing the folder.
+        old_folder_name (str): The current name of the folder.
+        new_folder_name (str): The new name for the folder.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
     if os.path.isdir(folder_path) == False:
         print(f'This folder path: {folder_path} does not exist')
         return
@@ -34,6 +74,17 @@ def rename_folder(folder_path, old_folder_name, new_folder_name):
         os.rename(f'{folder_path}/{old_folder_name}', f'{folder_path}/{new_folder_name}')
 
 def copy_folder(folder_path, folder_name, copied_folder_name):
+    """
+    Copies a folder to a new location.
+
+    Args:
+        folder_path (str): The path of the folder.
+        folder_name (str): The name of the folder to be copied.
+        copied_folder_name (str): The name of the copied folder.
+
+    Returns:
+        None
+    """
     if os.path.isdir(folder_path) == False:
         print(f'This folder path: {folder_path} does not exist')
         return
